27f7c3a3397a31300dd4dc2f8cd054cdef9332ad
Ambrosius <ambrosius411@protonmail.com>
2023-02-10 18:54:05 +0100

    Add function to generate computer choice

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
 function getComputerChoice () {
-    
+    let options = ["Rock", "Paper", "Scissors"]
+
+    let randomPick = options[randomIntFromInterval(0,2)]
+    return randomPick
 }
+
+
+
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+  
+  let computerChoice = getComputerChoice()
