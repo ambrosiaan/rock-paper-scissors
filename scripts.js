59fae6cbe68503cd09449326f90aa686946cd35c
Ambrosius <ambrosius411@protonmail.com>
2023-02-10 19:00:22 +0100

    Add function to test randomness computer choice

--- scripts.js
+++ scripts.js
@@ -12,3 +12,33 @@ function randomIntFromInterval(min, max) {
   }
   
   let computerChoice = getComputerChoice()
+  testRandomness()
+
+
+function testRandomness() {
+
+    let rockCounter = 0;
+    let paperCounter = 0;
+    let scissorsCounter = 0;
+    let n = 10000
+        for (i= 0; i<n; i++){
+            let computerChoice = getComputerChoice()
+            console.log(computerChoice)
+            switch (computerChoice) {
+                case "Rock":
+                rockCounter++
+                break;
+                case "Paper":
+                paperCounter++
+                break;
+                case "Scissors":
+                scissorsCounter++
+                break;
+                default:
+                exception("Invalid answer")
+            }
+        }
+    console.log(`In ${n} turns computer has chosen Rock ${rockCounter} times`)
+    console.log(`In ${n} turns computer has chosen Paper ${paperCounter} times`)
+    console.log(`In ${n} turns computer has chosen Scissors ${scissorsCounter} times`)
+}
